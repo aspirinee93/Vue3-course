ccff76d2a87af1e38fbe5f1c2d86b84c417e245f
aspirinee93 <zaitsevborisborisovich@gmail.com>
2022-11-22 21:44:28 +0500

    Добавление модального всплвающего окна

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="app">
-    <post-form-vue @create="createPost" />
-    <post-list-vue 
-      :posts="posts"
-      @remove="removePost"
-    />
+    <h1>Страница с поставми</h1>
+    <my-button-vue @click="showDialog">Создать пост</my-button-vue>
+    <my-dialog-vue v-model:show="dialogVisible">
+      <post-form-vue @create="createPost" />
+    </my-dialog-vue>
+    <post-list-vue :posts="posts" @remove="removePost" />
   </div>
 </template>
 
 <script>
 import PostListVue from "@/components/PostList.vue";
 import PostFormVue from "@/components/PostForm.vue";
+import MyDialogVue from "@/components/UI/MyDialog.vue";
+import MyButton from "./components/UI/MyButton.vue";
 
 export default {
   components: {
     PostFormVue,
     PostListVue,
+    MyDialogVue,
+    MyButton,
   },
   data() {
     return {
@@ -24,15 +29,20 @@ export default {
         { id: 2, title: "JavaScript2", body: "JS2 -  язык програмирования" },
         { id: 3, title: "JavaScript3", body: "JS3 -  язык програмирования" },
       ],
+      dialogVisible: false,
     };
   },
   methods: {
     createPost(post) {
-      this.posts.push(post)
+      this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
-      this.posts = this.posts.filter(p => p.id !== post.id)
-    }
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
   },
 };
 </script>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,7 +1,5 @@
 import MyButtonVue from "@/components/UI/MyButton";
 import MyInputVue from "@/components/UI/MyInput";
+import MyDialogVue from "@/components/UI/MyDialog";
 
-export default [
-  MyButtonVue,
-  MyInputVue
-];
+export default [MyButtonVue, MyInputVue, MyDialogVue];
